635d069f5d3d7a4e16d072b57bac9f25c12070d3
Jun Anthony Calupcupan <junanthony.business@gmail.com>
2024-03-06 22:01:23 +1300

    Add Customer Model

---
+++ MVC_Customer/Models/Customer.cs
@@ -0,0 +1,10 @@
+﻿namespace MVC_Customer.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<Item> Items { get; set; }
+    }
+}
