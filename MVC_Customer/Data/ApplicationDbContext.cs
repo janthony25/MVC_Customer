0f402c03db63ab806f507f01112e12845baabea3
Jun Anthony Calupcupan <junanthony.business@gmail.com>
2024-03-06 22:04:12 +1300

    Add ApplicationDbContext

---
+++ MVC_Customer/Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC_Customer.Models;
+
+namespace MVC_Customer.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Item> Items { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("YourConnectionString");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Define foreign key relationship
+            modelBuilder.Entity<Item>()
+                .HasOne<Customer>(i => i.Customer)
+                .WithMany(c => c.Items)
+                .HasForeignKey(i => i.CustomerId)
+                .IsRequired();
+        }
+    }
+}
