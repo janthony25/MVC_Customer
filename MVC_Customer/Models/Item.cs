3a6f44453d2e8f8aa39d4868dee6f472126cdd79
Jun Anthony Calupcupan <junanthony.business@gmail.com>
2024-03-06 22:02:36 +1300

    Add Item Model

---
+++ MVC_Customer/Models/Item.cs
@@ -0,0 +1,11 @@
+﻿namespace MVC_Customer.Models
+{
+    public class Item
+    {
+        public int ItemId { get; set; }
+        public string Name { get; set; }
+
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+    }
+}
